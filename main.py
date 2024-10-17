50763ff96374c06b29249c3f5f8d34b83fb2740b
Sevastyan <6632768420@my.lerna.kz>
2024-10-17 15:06:00 +0500

    deleted files

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
-# main.py
 import telebot
-from handlers.lowprice import setup_lowprice_handler
+from dotenv import load_dotenv
+import os
+from handlers.start_handler import handle_start
+from handlers.help_handler import handle_help
+from handlers.lowprice_handler import handle_lowprice
 
-# Токен
-TELEGRAM_TOKEN = "7873288430:AAG0sqt7Mh4QNWIy0XtRGyyMTrZpDO_S_MA"
+# Загрузка конфигурации из .env
+load_dotenv()
+API_TOKEN = os.getenv("TELEGRAM_API_TOKEN")
 
-# Инициализация бота
-bot = telebot.TeleBot(TELEGRAM_TOKEN)
+bot = telebot.TeleBot(API_TOKEN)
 
-# Подключение обработчиков команд
-setup_lowprice_handler(bot)
 
-# Запуск бота
+# Обработчик команды /start
+@bot.message_handler(commands=["start"])
+def start_command(message):
+    handle_start(bot, message)
+
+
+# Обработчик команды /help
+@bot.message_handler(commands=["help"])
+def help_command(message):
+    handle_help(bot, message)
+
+
+# Обработчик команды /lowprice
+@bot.message_handler(commands=["lowprice"])
+def lowprice_command(message):
+    handle_lowprice(bot, message)
+
+
 if __name__ == "__main__":
     bot.polling(none_stop=True)

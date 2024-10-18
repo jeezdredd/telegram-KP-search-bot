7721d27d9dd831e52347eab909a87eba79d74806
Sevastyan <6632768420@my.lerna.kz>
2024-10-18 11:29:02 +0500

    created simple echo bot that repeats the users words.

---
+++ main.py
@@ -0,0 +1,13 @@
+import config_data.config as config
+import telebot
+
+bot = telebot.TeleBot(config.TELEGRAM_TOKEN)
+
+
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message):  # Название функции не играет никакой роли
+    bot.send_message(message.chat.id, message.text)
+
+
+if __name__ == "__main__":
+    bot.infinity_polling()
